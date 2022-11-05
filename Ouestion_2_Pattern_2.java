88c067e49f6c7a7c98fe1c1dfe55384ea4d1d72a
Perumal-nadar <116861303+Perumal-nadar@users.noreply.github.com>
2022-11-05 14:03:09 +0530

    Update Ouestion_2_Pattern_2.java

--- Ouestion_2_Pattern_2.java
+++ Ouestion_2_Pattern_2.java
@@ -1,15 +1,22 @@
 package T_lab;
-
+/*
+*********
+*******
+*****
+***
+*
+*/
 public class Ouestion_2_Pattern_2 {
 
 	public static void main(String[] args) {
 		int i,j;
 		
-		for(i=1;i<=5;i++) {
-			for(j=1;j<=i*2-1;j++){
+		for(i=1;i>=5;i++) {
+			for(j=1;j<=row*2-1;j++) {
 				System.out.print("*");
 			}
-			System.out.println();
+			
+		System.out.println();
 		}
 
 	}
