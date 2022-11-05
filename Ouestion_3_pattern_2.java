94955a13c3f39b59c206e128ded5e6d537276cdc
Perumal-nadar <116861303+Perumal-nadar@users.noreply.github.com>
2022-11-05 14:28:08 +0530

    Update Ouestion_3_pattern_2.java

--- Ouestion_3_pattern_2.java
+++ Ouestion_3_pattern_2.java
@@ -1,14 +1,21 @@
 package T_lab;
+/*
 
+******
+*    *
+*    *
+******
+
+*/
 public class Ouestion_3_pattern_2 {
 
 	public static void main(String[] args) {
 		int i;
-		for(i=1;i<=4;i++) {
-			if(i==1 || i==4) {
+		for(i=1;i<=4;i++) { // Condition Required for Column...
+ 			if(i==1 || i==4) { // As the 1st and 4th rows are similar.....
 				System.out.println("******");
 			}
-			else if(i==2 || i==3) {
+			else if(i==2 || i==3) { // As the 2nd and 3rd rows are similar.....
 				System.out.println("*    *");
 			}
 		}
